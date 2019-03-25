d5cccb9087f5fba753f8173d0b59ecb68faf5a4a
Chetan <parmar.chetan91@gmail.com>
2019-03-26 00:32:39 +0100

    express example

---
+++ webapp/app.js
@@ -0,0 +1,24 @@
+var express= require('express');
+var path=require('path');
+var bodyParser=require('body-parser');
+var nodemailer=require('nodemailer');
+
+
+var app = express();
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.get('/',function(req,res){
+	
+	res.render('index');
+});
+app.get('/about',function(req,res){
+	
+	res.render('layout');
+});
+app.listen(3000);
+console.log("server is ruuning on port 3000");
